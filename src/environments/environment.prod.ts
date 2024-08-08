b98c432d7b1d1be2ea06113fec6b7c8efbd0366a
rsi-gaurav <gaurav.agarwal1@rsystems.com>
2024-08-08 07:37:43 -0500

    Add files via upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hackernewsbackend-dev-eqc3azabfbhhbfh2.centralindia-01.azurewebsites.net',
+  apiUrl: 'https://localhost:5000',
 };
