a0144fe500172160d83c68b7a9f3e877559d2600
rsi-gaurav <gaurav.agarwal1@rsystems.com>
2024-08-12 05:11:39 -0500

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('hacker-news-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hacker-news-app app is running!');
-  });
 });
